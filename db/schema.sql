7a41e18e01578ea54bb15ea6a15451ff272181ec
Alec Taber <alectaber12@gmail.com>
2024-08-27 14:07:23 -0700

    add tables to schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary NUMERIC NOT NULL,
+    department_id INTEGER REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INTEGER REFERENCES roles(id),
+    manager_id INTEGER REFERENCES employees(id) ON DELETE SET NULL
+);
